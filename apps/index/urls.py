bd1186ade6dae00dc10fecbccb4859fb52ff3250
lightfisher <201030049@qq.com>
2018-12-19 22:42:03 +0800

    music网站

---
+++ apps/index/urls.py
@@ -0,0 +1,8 @@
+# index 的urls.py
+from django.urls import path
+from . import views
+
+# 设置首页的URL地址信息
+urlpatterns = [
+    path('', views.indexView, name='index'),
+]
---
+++ try.py
@@ -0,0 +1,3 @@
+import urllib.request
+
+urllib.request.urlretrieve('https://m10.music.126.net/20181217224600/3f2e9586f7eba68a5c57e4f8ac09070c/ymusic/ace9/1b15/fafa/4f228c429266fb85f36e7eb2e2374c96.mp3',r'E:\python_project\django\music\1.m4a')
